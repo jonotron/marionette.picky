3f197285f1dfdebd851ce5923dc5087df48b22c4
Jonathan Bowers <jonotron@gmail.com>
2013-04-19 16:01:28 -0700

    Test that #select() triggers 'selected' and calls #onSelected()

--- spec/selectable.spec.js
+++ spec/selectable.spec.js
@@ -18,5 +18,41 @@ describe('Selectable', function() {
       selectable.select();
       expect(selectable.selected).to.equal(true);
     });
+
+    it("triggers 'selected'", function() {
+      // no-op trigger
+      selectable.trigger = function() {};
+      var spy = sinon.spy(selectable, 'trigger'); 
+
+      selectable.select();
+      expect(spy).to.have.been.calledWith('selected', selectable);
+    });
+
+    it("calls 'onSelected'", function() {
+      // no-op onSelected
+      selectable.onSelected = function() {};
+      var spy = sinon.spy(selectable, 'onSelected'); 
+
+      selectable.select();
+      expect(spy).to.have.been.called;
+    });
+
+    it("does not trigger anything if already true", function() {
+      selectable.trigger = function() {};
+      var spy = sinon.spy(selectable, 'trigger');
+
+      selectable.selected = true;
+      selectable.select();
+      expect(spy).not.to.have.been.called;
+    });
+
+    it("does not call onSelected already true", function() {
+      selectable.onSelected = function() {};
+      var spy = sinon.spy(selectable, 'onSelected');
+
+      selectable.selected = true;
+      selectable.select();
+      expect(spy).not.to.have.been.called;
+    });
   });
 });
