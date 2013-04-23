55d638ad9b45fab0ac6f5f227281183360b85c87
Jonathan Bowers <jonotron@gmail.com>
2013-04-22 20:49:49 -0700

    MultiSelect should have an empty selected array on initialization

---
+++ spec/multiselect.spec.js
@@ -0,0 +1,54 @@
+describe("MultiSelect", function() {
+  var multi;
+  var selectable;
+  var selectable2;
+  var selectable3;
+
+  beforeEach(function() {
+    // mixin onto a simple object
+    multi = _.extend({}, PickleMixins.MultiSelect);
+
+    // I've chosen not to mock up the selectable, as it just seems
+    // like more work. Instead I get an actual selectable and spy
+    // on it
+    selectable  = _.extend({}, PickleMixins.Selectable);
+    selectable2 = _.extend({}, PickleMixins.Selectable);
+    selectable3 = _.extend({}, PickleMixins.Selectable);
+  });
+
+  describe("when first created", function() {
+    it("should have an empty default selected array", function() {
+      expect(multi.selected).to.be.an('array').and.to.be.empty; 
+    });
+  }); // when first created
+
+  describe("when selecting an item with #selectItem()", function() {
+    it("should call the selectable's #select() method", function() {
+      var spy = sinon.spy(selectable, 'select');
+      singleselect.selectItem(selectable); 
+      
+      expect(selectable.select).to.have.been.called;
+    });
+
+    it("should call #onSelectItem() if defined", function() {
+      // no-op onSelectItem
+      singleselect.onSelectItem = function() {};
+      var spy = sinon.spy(singleselect, 'onSelectItem'); 
+
+      singleselect.selectItem(selectable);
+      expect(singleselect.onSelectItem).to.have.been.calledWith(selectable);
+    });
+
+    it.skip("should add the selected item to the list", function() {
+    });
+
+    it.skip("should add all selected items to the list", function() {
+    });
+
+    describe("when a previous item is already selected", function() {
+      it.skip("should not try calling #deselect()", function() {
+      });
+    }); // when a previous item is already selected
+  }); // when selecting an item with #selectItem()
+
+}); // SingleSelect
--- src/multiselect.js
+++ src/multiselect.js
@@ -15,32 +15,8 @@
   }
 
   MultiSelect = PickleMixins.MultiSelect = {
-    selectedLength: 0,
     selected: [],
 
-    selectItem: function(item) {
-      // escape early if we try to select something already selected
-      if (this.selected === item) return;
-
-      // deselect previously selected
-      if (this.selected) this.selected.deselect();
-
-      this.selected = item;
-      item.select(); 
-
-      if (typeof this.onSelectItem === 'function')
-        this.onSelectItem(item);
-    },
-
-    deselectItem: function(item) {
-      if (this.selected !== item) return;
-
-      this.selected = null;
-      item.deselect();
-
-      if (typeof this.onDeselectItem === 'function')
-        this.onDeselectItem(item);
-    }
   }
 
 }).call(this);
